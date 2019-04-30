7f350eb57f17551e1bf6c30ae784fa6ad0dd9ad6
MartinCJ08 <martin.carrascoj@gmail.com>
2019-04-30 00:24:36 -0600

    Cuadro de dialgo no funcional

--- cafeApp/app/src/main/java/com/example/cafeapp/Activities/ListaProductosActivity.java
+++ cafeApp/app/src/main/java/com/example/cafeapp/Activities/ListaProductosActivity.java
@@ -1,9 +1,12 @@
 package com.example.cafeapp.Activities;
 
+import android.app.Dialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -48,29 +51,63 @@ public class ListaProductosActivity extends AppCompatActivity implements ListVie
             new Producto("Torta de chile relleno",30,6),
             new Producto("Torta de lomo",35,6)
     };
+    Producto[] currentArrayP;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_productos);
         idMenu = getIntent().getIntExtra("idMenu",1);
+        currentArrayP = getProductByMenu(apProd,idMenu);
 
         txtVwTitleMenu = findViewById(R.id.txtVwTitleMenu);
         lstProd = findViewById(R.id.lstProd);
         lstProd.setAdapter(
-                new ListaProductosAdapter(this,R.layout.products_row,getProductByMenu(apProd,idMenu))
+                new ListaProductosAdapter(this,R.layout.products_row,currentArrayP)
         );
 
         lstProd.setOnItemClickListener(this);
+    }
 
-
+    public void onClick(View v){
+        switch (v.getId()){
+            case R.id.btnAddMore:
+                finish();
+                break;
+            case R.id.btnFinalizar:
+                break;
+        }
     }
 
     @Override
-    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-        //TODO: Cuadro de dialogo para agregar al carrito
-        Toast.makeText(getApplicationContext(),"TODO:Cuadro de dialogo",Toast.LENGTH_SHORT).show();
+    public void onItemClick(AdapterView<?> parent, View view, final int position, long id) {
+        final Dialog dlgMiDialogo;
+        dlgMiDialogo = new Dialog(this);
+        dlgMiDialogo.setContentView(R.layout.dialog_add_product);
+        final EditText edtTxtCant;
+        Button btnAddProduct;
+
+        edtTxtCant = dlgMiDialogo.findViewById(R.id.edtTxtCant);
+        btnAddProduct = dlgMiDialogo.findViewById(R.id.btnAddProduct);
+
+        btnAddProduct.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),"Number"+edtTxtCant.getText().toString()+
+                        " Producto:"+currentArrayP[position].getNomProd(),Toast.LENGTH_SHORT).show();
+                dlgMiDialogo.dismiss();
+            }
+        });
+
+        dlgMiDialogo.show();
     }
 
+    /**
+     *
+     * @param apData Arreglo de productos inicial
+     * @param iCategoria id de categoria que se desea agrupar
+     * @return regresa un arreglo de productos agrupados por menu
+     */
     public Producto[] getProductByMenu(Producto[] apData,int iCategoria){
         ArrayList<Producto> arrayAux = new ArrayList<Producto>();
         for (int i = 0; i<apData.length;i++){
